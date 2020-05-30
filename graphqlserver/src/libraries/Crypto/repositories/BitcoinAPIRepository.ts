1c5bbf301bce9a5e78549985e5b061f507789b6c
netpoe <gus@simpleico.com>
2020-05-30 21:50:01 +0200

    Updates FiatToCrypto breakdown results to default GTQ conversion

--- graphqlserver/src/libraries/Crypto/repositories/BitcoinAPIRepository.ts
+++ graphqlserver/src/libraries/Crypto/repositories/BitcoinAPIRepository.ts
@@ -5,7 +5,12 @@ export class BitcoinAPIRepository implements IBitcoinAPIRepository {
   async getCurrentPriceByCurrencySymbol(
     symbol: CurrencySymbol = CurrencySymbol.USD,
   ): Promise<string> {
-    return "7200";
+    switch (symbol) {
+      case CurrencySymbol.GTQ:
+        return (7200 * 7.5).toString();
+      default:
+        return "7200";
+    }
   }
 
   async getPriceAtDatetimeByCurrencySymbol(
