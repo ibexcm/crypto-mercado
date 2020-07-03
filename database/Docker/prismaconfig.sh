9b5fb9d8c04b7e7ef1bf246dcfe955ea5a0e37f9
susguzman <susguzman@gmail.com>
2020-07-03 13:48:19 -0600

    cambios buildspec

--- database/Docker/prismaconfig.sh
+++ database/Docker/prismaconfig.sh
@@ -2,10 +2,10 @@
 
 # Setting prisma variables
 echo "Setting prisma variables"
-sed -i "s/__MANAGEMENT_API_SECRET__/$(_R=$(echo $SECRET | ./jq -r '.PRISMA_MANAGEMENT_API_SECRET'); [ $_R == null ] || [ -z $_R ] && echo $MANAGEMENT_API_SECRET || echo $_R)/g" prismaconfig.yml
+sed -i "s/__MANAGEMENT_API_SECRET__/$(_R=$(echo $SECRET | ./jq -r '.PRISMA_MANAGEMENT_API_SECRET'); [ $_R == null ] || [ -z $_R ] && echo $PRISMA_MANAGEMENT_API_SECRET || echo $_R)/g" prismaconfig.yml
 sed -i "s/__PORT__/$(_R=$(echo $SECRET | ./jq -r '.PRISMA_PORT'); [ $_R == null ] || [ -z $_R ] && echo $PRISMA_PORT || echo $_R)/g" prismaconfig.yml
-sed -i "s/__DATABASE_CONNECTOR__/$(_R=$(echo $SECRET | ./jq -r '.CONNECTOR'); [ $_R == null ] || [ -z $_R ] && echo $CONNECTOR || echo $_R)/g" prismaconfig.yml 
-sed -i "s/__ENABLE_DB_MIGRATIONS__/$(_R=$(echo $SECRET | ./jq -r '.MIGRATION'); [ $_R == null ] || [ -z $_R ] && echo $MIGRATION || echo $_R)/g" prismaconfig.yml 
+sed -i "s/__DATABASE_CONNECTOR__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_CONNECTOR'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_CONNECTOR || echo $_R)/g" prismaconfig.yml 
+sed -i "s/__ENABLE_DB_MIGRATIONS__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_MIGRATION'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_MIGRATION || echo $_R)/g" prismaconfig.yml 
 sed -i "s/__DATABASE_HOST__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_HOST'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_HOST || echo $_R)/g" prismaconfig.yml
 sed -i "s/__DATABASE_PORT__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_PORT'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_PORT || echo $_R)/g" prismaconfig.yml
 sed -i "s/__DATABASE_USER__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_USER'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_USER || echo $_R)/g" prismaconfig.yml
