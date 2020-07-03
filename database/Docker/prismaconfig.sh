aa84c00c5048359382f43684e227f06f48df94db
susguzman <susguzman@gmail.com>
2020-07-03 13:00:55 -0600

    cambio secrets manager

--- database/Docker/prismaconfig.sh
+++ database/Docker/prismaconfig.sh
@@ -1,15 +1,17 @@
 #/bin/bash
 
 # Setting prisma variables
-sed -i "s/__MANAGEMENT_API_SECRET__/$MANAGEMENT_API_SECRET/g" prismaconfig.yml
-sed -i "s/__PORT__/$PRISMA_PORT/g" prismaconfig.yml
-sed -i "s/__DATABASE_CONNECTOR__/$CONNECTOR/g" prismaconfig.yml 
-sed -i "s/__ENABLE_DB_MIGRATIONS__/$MIGRATION/g" prismaconfig.yml 
-sed -i "s/__DATABASE_HOST__/$DATABASE_HOST/g" prismaconfig.yml
-sed -i "s/__DATABASE_PORT__/$DATABASE_PORT/g" prismaconfig.yml
-sed -i "s/__DATABASE_USER__/$DATABASE_USER/g" prismaconfig.yml
-sed -i "s/__DATABASE_PASSWORD__/$DATABASE_PASSWORD/g" prismaconfig.yml 
-sed -i "s/__DATABASE_NAME__/$DATABASE_NAME/g" prismaconfig.yml 
+echo "Setting prisma variables"
+sed -i "s/__MANAGEMENT_API_SECRET__/$(_R=$(echo $SECRET | ./jq -r '.PRISMA_MANAGEMENT_API_SECRET'); [ $_R == null ] || [ -z $_R ] && echo $MANAGEMENT_API_SECRET || echo $_R)/g" prismaconfig.yml
+sed -i "s/__PORT__/$(_R=$(echo $SECRET | ./jq -r '.PRISMA_PORT'); [ $_R == null ] || [ -z $_R ] && echo $PRISMA_PORT || echo $_R)/g" prismaconfig.yml
+sed -i "s/__DATABASE_CONNECTOR__/$(_R=$(echo $SECRET | ./jq -r '.CONNECTOR'); [ $_R == null ] || [ -z $_R ] && echo $CONNECTOR || echo $_R)/g" prismaconfig.yml 
+sed -i "s/__ENABLE_DB_MIGRATIONS__/$(_R=$(echo $SECRET | ./jq -r '.MIGRATION'); [ $_R == null ] || [ -z $_R ] && echo $MIGRATION || echo $_R)/g" prismaconfig.yml 
+sed -i "s/__DATABASE_HOST__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_HOST'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_HOST || echo $_R)/g" prismaconfig.yml
+sed -i "s/__DATABASE_PORT__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_PORT'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_PORT || echo $_R)/g" prismaconfig.yml
+sed -i "s/__DATABASE_USER__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_USER'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_USER || echo $_R)/g" prismaconfig.yml
+sed -i "s/__DATABASE_PASSWORD__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_PASSWORD'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_PASSWORD || echo $_R)/g" prismaconfig.yml 
+sed -i "s/__DATABASE_NAME__/$(_R=$(echo $SECRET | ./jq -r '.DATABASE_NAME'); [ $_R == null ] || [ -z $_R ] && echo $DATABASE_NAME || echo $_R)/g" prismaconfig.yml 
 
 # Set enviroment variable
+echo "Setting PRISMA_CONFIG"
 export PRISMA_CONFIG=$(cat prismaconfig.yml)
