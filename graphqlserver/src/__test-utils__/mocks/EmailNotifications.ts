0664a65357e85383915c2c310d5f7195b230c96c
Gus <gus@ibexmercado.com>
2020-08-17 10:43:36 -0600

    Updates types

--- graphqlserver/src/__test-utils__/mocks/EmailNotifications.ts
+++ graphqlserver/src/__test-utils__/mocks/EmailNotifications.ts
@@ -1,7 +1,22 @@
+import { Transaction } from "@ibexcm/database";
 import { IEmailNotificationsRepository } from "../../libraries/EmailVerification/interfaces/IEmailNotificationsRepository";
 
 export const mockEmailNotificationsRepository = (): IEmailNotificationsRepository => {
   return {
     sendAdminKYCApproveUserNotification: jest.fn((address: string) => Promise.resolve()),
+    sendTransactionSuccessNotification: jest.fn(
+      (
+        address: string,
+        {
+          transaction,
+          fromCurrencySymbol,
+          toCurrencySymbol,
+        }: {
+          transaction: Transaction;
+          fromCurrencySymbol: string;
+          toCurrencySymbol: string;
+        },
+      ) => Promise.resolve(),
+    ),
   };
 };
