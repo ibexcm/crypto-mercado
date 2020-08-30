328f91ab26a60b07f4a5d0e4049ecb066f7380ce
Ktoxcon <kennethtoxro@gmail.com>
2020-08-30 17:54:52 -0600

    SMSAccountRecovery Repository Injection Key added

--- graphqlserver/src/libraries/SMSVerification/InjectionKeys.ts
+++ graphqlserver/src/libraries/SMSVerification/InjectionKeys.ts
@@ -1,9 +1,17 @@
 import { InjectionKey, InjectionKeyScope } from "@ibexcm/libraries/di";
-import { ISMSVerificationRepository } from "./interfaces/ISMSVerificationRepository";
-import { SMSVerificationRepository } from "./repository/SMSVerificationRepository";
+import { SMSVerificationRepository } from "./repository";
+import { SMSAccountRecoveryRepository } from "./repository";
+import { ISMSVerificationRepository } from "./interfaces";
+import { ISMSAccountRecoveryRepository } from "./interfaces";
 
 export const smsVerificationRepositoryInjectionKey: InjectionKey<ISMSVerificationRepository> = {
   name: "smsVerificationRepository",
   scope: InjectionKeyScope.singleton,
   closure: _ => SMSVerificationRepository,
+};
+
+export const smsAccountRecoveryRepositoryInjectionKey: InjectionKey<ISMSAccountRecoveryRepository> = {
+  name: "smsAccounRecoveryRepository",
+  scope: InjectionKeyScope.singleton,
+  closure: _ => SMSAccountRecoveryRepository,
 };
