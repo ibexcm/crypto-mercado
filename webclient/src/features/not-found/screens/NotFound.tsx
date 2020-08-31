75c79c7bbc1e83c77c553a94154f14d7cc948e03
Ktoxcon <kennethtoxro@gmail.com>
2020-08-30 18:13:26 -0600

    Not Found Screen added

---
+++ webclient/src/features/not-found/screens/NotFound.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import DependencyContext from "../../../common/contexts/DependencyContext";
+import { styles } from "../../../common/theme";
+import { MobileNavBar, NavBar } from "../components";
+import { ToolbarPadding, Typography } from "../../../common/components";
+import { RouteComponentProps, StaticContext } from "react-router";
+import { Box, Container, Theme, withStyles, WithStyles } from "@material-ui/core";
+
+interface Props extends WithStyles, RouteComponentProps<{}, StaticContext> {}
+
+const Component: React.FC<Props> = ({ classes, history, location, match, ...props }) => {
+  const dependencies = React.useContext(DependencyContext);
+
+  return (
+    <Box className={classes.homeContainer}>
+      <MobileNavBar />
+      <NavBar />
+      <Container maxWidth="xs">
+        <Box>
+          <ToolbarPadding />
+          <Box>
+            <Typography variant="h5" mb={2}>
+              Lo sentimos, No pudimos Encontrar la Página.
+            </Typography>
+            <img src="/svg/not-found.svg" alt="Not Found Image" />
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export const NotFound = withStyles((theme: Theme) => ({
+  ...styles(theme),
+  homeContainer: {
+    backgroundColor: "white",
+  },
+}))(Component);
