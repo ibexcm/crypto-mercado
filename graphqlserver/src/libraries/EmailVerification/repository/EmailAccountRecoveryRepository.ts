a89c3bff3c05d5965a3684ef9d51abcf4986ba7e
Ktoxcon <kennethtoxro@gmail.com>
2020-08-31 14:09:20 -0600

    Error Handling added

--- graphqlserver/src/libraries/EmailVerification/repository/EmailAccountRecoveryRepository.ts
+++ graphqlserver/src/libraries/EmailVerification/repository/EmailAccountRecoveryRepository.ts
@@ -1,5 +1,6 @@
 import config from "../../../config";
 import sendgridClient from "../service/sendgridClient";
+import { EmailAccountRecoveryError } from "../errors";
 import { IEmailAccountRecoveryRepository } from "../interfaces";
 
 const { host, from } = config.get("email");
@@ -21,7 +22,42 @@ const sendRecoveryLink: IEmailAccountRecoveryRepository["sendRecoveryLink"] = as
         reset_password_url: `${host}/restablecer-contraseña?t=${token}`,
       },
     });
-  } catch (error) {}
+  } catch (error) {
+    switch (error.code) {
+      case 403:
+        throw EmailAccountRecoveryError.unauthorizedError;
+        break;
+      case 421:
+        throw EmailAccountRecoveryError.messagesTemporarilyDefferedError;
+        break;
+      case 450:
+        throw EmailAccountRecoveryError.tooManyConnectionsError;
+        break;
+      case 451:
+        throw EmailAccountRecoveryError.maximumCreditsExceededError;
+        break;
+      case 452:
+        throw EmailAccountRecoveryError.insufficientLocalStorageError;
+        break;
+      case 550:
+        throw EmailAccountRecoveryError.mailboxUnavailableError;
+        break;
+      case 551:
+        throw EmailAccountRecoveryError.userDoesNotExistsError;
+        break;
+      case 552:
+        throw EmailAccountRecoveryError.messageSizeExceededError;
+        break;
+      case 553:
+        throw EmailAccountRecoveryError.invalidUserError;
+        break;
+      case 554:
+        throw EmailAccountRecoveryError.mailRefusedError;
+        break;
+      default:
+        throw error;
+    }
+  }
 };
 
 export const EmailAccountRecoveryRepository: IEmailAccountRecoveryRepository = {
