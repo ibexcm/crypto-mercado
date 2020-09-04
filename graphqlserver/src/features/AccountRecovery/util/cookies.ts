244b0469946da7a50651a40fa0c5579f37623324
Ktoxcon <kennethtoxro@gmail.com>
2020-09-04 14:17:12 -0600

    CookieOptions expiresAt added

--- graphqlserver/src/features/AccountRecovery/util/cookies.ts
+++ graphqlserver/src/features/AccountRecovery/util/cookies.ts
@@ -1,14 +1,20 @@
 import { CookieOptions } from "express-serve-static-core";
+import { DateTime } from "luxon";
 import ms from "ms";
 
 export async function createSecureCookieOptions({
   duration = "15m",
 }: { duration?: string } = {}): Promise<CookieOptions> {
   const expiresIn = ms(duration);
+  const expiresAt = new Date(
+    DateTime.utc()
+      .plus({ milliseconds: expiresIn })
+      .toISO(),
+  );
   const cookieSecureOptions: CookieOptions = {
     httpOnly: true,
     maxAge: expiresIn,
-    expires: new Date(Date.now() + expiresIn),
+    expires: expiresAt,
   };
 
   return cookieSecureOptions;
