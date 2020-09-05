fd312a75d38054aff88296ac403e66493b32c2c9
Ktoxcon <kennethtoxro@gmail.com>
2020-09-05 17:28:03 -0600

    queries and mutations added to index

--- graphqlserver/src/features/AccountRecovery/resolvers/index.ts
+++ graphqlserver/src/features/AccountRecovery/resolvers/index.ts
@@ -1,3 +1,10 @@
-export * from "./resetPassword";
-export * from "./sendEmailAccountRecovery";
-export * from "./sendSmsAccountRecovery";
+import { recoverAccount } from "./recoverAccount";
+import { resetPassword } from "./resetPassword";
+
+export const queries = {
+  recoverAccount,
+};
+
+export const mutations = {
+  resetPassword,
+};
