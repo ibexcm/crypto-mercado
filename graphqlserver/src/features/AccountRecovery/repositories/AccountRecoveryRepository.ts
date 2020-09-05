0cdfdbe74c8cc61d034924188dcaab1ea41872b9
Ktoxcon <kennethtoxro@gmail.com>
2020-09-05 17:29:38 -0600

    AccountRecoveryRepository  updated

--- graphqlserver/src/features/AccountRecovery/repositories/AccountRecoveryRepository.ts
+++ graphqlserver/src/features/AccountRecovery/repositories/AccountRecoveryRepository.ts
@@ -1,89 +1,87 @@
 import { Prisma, User } from "@ibexcm/database";
-import {
-  MutationResetPasswordArgs,
-  MutationSendEmailAccountRecoveryArgs,
-  MutationSendSmsAccountRecoveryArgs,
-} from "@ibexcm/libraries/api";
+import { MutationResetPasswordArgs, QueryRecoverAccountArgs } from "@ibexcm/libraries/api";
 import { genSalt, hash } from "bcryptjs";
+import { ICookiesGenRepository } from "../../../libraries/Cookies";
 import { IEmailAccountRecoveryRepository } from "../../../libraries/EmailVerification";
 import { ISessionRepository } from "../../../libraries/Session";
 import { ISMSAccountRecoveryRepository } from "../../../libraries/SMSVerification";
+import { IContext } from "../../../server/interfaces/IContext";
 
 export class AccountRecoveryRepository {
   private db: Prisma;
   private sessionRepository: ISessionRepository;
+  private cookiesGenRepository: ICookiesGenRepository;
   private emailAccountRecoveryRepository: IEmailAccountRecoveryRepository;
   private smsAccountRecoveryRepository: ISMSAccountRecoveryRepository;
 
   constructor(
     db: Prisma,
     sessionRepository: ISessionRepository,
+    cookiesGenRepository: ICookiesGenRepository,
     emailAccountRecoveryRepository: IEmailAccountRecoveryRepository,
     smsAccountRecoveryRepository: ISMSAccountRecoveryRepository,
   ) {
     this.db = db;
     this.sessionRepository = sessionRepository;
+    this.cookiesGenRepository = cookiesGenRepository;
     this.emailAccountRecoveryRepository = emailAccountRecoveryRepository;
     this.smsAccountRecoveryRepository = smsAccountRecoveryRepository;
   }
 
-  async sendEmailAccountRecovery({
-    args: { address },
-  }: MutationSendEmailAccountRecoveryArgs) {
-    try {
-      const _user = await this.db
+  async recoverAccount(
+    {
+      args: {
+        address: { address },
+        number: { number },
+      },
+    }: QueryRecoverAccountArgs,
+    response: IContext["response"],
+  ) {
+    let user;
+
+    if (Boolean(address)) {
+      user = await this.db
         .email({ address })
         .contact()
         .user();
-
-      const { token } = await this.sessionRepository.createAccountRecoverySession(_user);
-      const emailSent = await this.emailAccountRecoveryRepository.sendRecoveryLink(
-        address,
-        {
-          token,
-        },
-      );
-      return {
-        token,
-        emailSent,
-      };
-    } catch (error) {}
-  }
-
-  async sendSMSAccountRecovery({ args: { number } }: MutationSendSmsAccountRecoveryArgs) {
-    try {
-      const _user = await this.db
+    } else if (Boolean(number)) {
+      user = await this.db
         .phoneNumber({ number })
         .contact()
         .user();
+    }
 
-      const { token } = await this.sessionRepository.createAccountRecoverySession(_user);
-      const smsSent = await this.smsAccountRecoveryRepository.sendRecoveryLink(number, "", {
-        token,
-      });
+    const { token, expiresAt } = await this.sessionRepository.createAccountRecoverySession(
+      user,
+    );
 
-      return {
-        token,
-        smsSent,
-      };
-    } catch (error) {}
+    if (Boolean(address))
+      await this.emailAccountRecoveryRepository.sendRecoveryLink(address, { token });
+    else if (Boolean(number))
+      await this.smsAccountRecoveryRepository.sendRecoveryLink(number, { token });
+
+    const cookieValues = await this.cookiesGenRepository.createCookie(token);
+    response.cookie(...cookieValues);
+
+    return {
+      token,
+      expiresAt,
+    };
   }
 
   async resetPassword({ args: { password } }: MutationResetPasswordArgs, user: User) {
-    try {
-      const _user = await this.db.updateUser({
-        where: {
-          id: user.id,
-        },
-        data: {
-          account: {
-            update: {
-              password: await hash(password, await genSalt()),
-            },
+    const _user = await this.db.updateUser({
+      where: {
+        id: user.id,
+      },
+      data: {
+        account: {
+          update: {
+            password: await hash(password, await genSalt()),
           },
         },
-      });
-      return await this.sessionRepository.createAuthenticationSession(_user);
-    } catch (error) {}
+      },
+    });
+    return await this.sessionRepository.createAuthenticationSession(_user);
   }
 }
