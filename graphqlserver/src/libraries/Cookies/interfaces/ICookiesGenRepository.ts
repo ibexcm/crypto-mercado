3e94accd024df467ff17be91d3e73d72fd00f975
Ktoxcon <kennethtoxro@gmail.com>
2020-09-08 11:49:01 -0600

    CookieGen Lib removed

--- graphqlserver/src/libraries/Cookies/interfaces/ICookiesGenRepository.ts
+++
@@ -1,8 +0,0 @@
-import { CookieOptions } from "express-serve-static-core";
-
-export type Cookie = [string, any, CookieOptions];
-
-export interface ICookiesGenRepository {
-  createCookie(value: any, name?: string, secOptions?: boolean): Promise<Cookie>;
-  secureOptions(expiration?: string): Promise<CookieOptions>;
-}
