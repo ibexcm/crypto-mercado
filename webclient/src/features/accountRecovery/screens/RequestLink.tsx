18d39819fc895a55fe719875f1eb3e4954f87f31
Ktoxcon <kennethtoxro@gmail.com>
2020-09-14 22:22:05 -0600

    RequestLink screen updated, new features added

---
+++ webclient/src/features/accountRecovery/screens/RequestLink.tsx
@@ -0,0 +1,191 @@
+import React from "react";
+import DependencyContext from "../../../common/contexts/DependencyContext";
+import { styles } from "../../../common/theme";
+import { MobileNavBar, NavBar } from "../components";
+import { TabPanel, TabContext, TabList } from "@material-ui/lab";
+import { AccountRecoveryRepositoryInjectionKey } from "../InjectionKey";
+import { RouteComponentProps, StaticContext } from "react-router";
+import { Box, Tab, Container, Theme, withStyles, WithStyles } from "@material-ui/core";
+import {
+  Button,
+  InputErrorBox,
+  TextField,
+  ToolbarPadding,
+  Typography,
+} from "../../../common/components";
+import { QueryRecoverAccountArgs } from "@ibexcm/libraries/api";
+
+interface Props extends WithStyles, RouteComponentProps<{}, StaticContext> {}
+
+const Component: React.FC<Props> = ({ classes, history, location, match, ...props }) => {
+  const dependencies = React.useContext(DependencyContext);
+  const AccountRecoveryRepository = dependencies.provide(
+    AccountRecoveryRepositoryInjectionKey,
+  );
+
+  const [recoveryOption, setRecoveryOption] = React.useState("1");
+
+  const [emailError, setEmailError] = React.useState<Error | null>(null);
+  const [smsError, setSmsError] = React.useState<Error | null>(null);
+
+  const [emailInput, setEmailInput] = React.useState<QueryRecoverAccountArgs>({
+    args: {
+      emailRecovery: {
+        address: "",
+      },
+    },
+  });
+  const [smsInput, setSmsInput] = React.useState<QueryRecoverAccountArgs>({
+    args: {
+      smsRecovery: {
+        number: "+502",
+      },
+    },
+  });
+
+  const [
+    executeGetAccountRecoveryLink,
+    { data, loading, error },
+    setAuthToken,
+  ] = AccountRecoveryRepository.useGetAccountRecoveryLink();
+
+  const onSendLink = async () => {
+    try {
+      if (isByEmail()) {
+        setEmailError(null);
+        await executeGetAccountRecoveryLink(emailInput);
+      } else {
+        setSmsError(null);
+        await executeGetAccountRecoveryLink(smsInput);
+      }
+
+      setAuthToken(data.recoverAccount.token);
+    } catch (err) {
+      if (isByEmail()) {
+        setEmailError(err);
+      } else {
+        setSmsError(err);
+      }
+    }
+  };
+
+  const handleRecoveryOptionChange = (event: React.ChangeEvent<{}>, newOption: string) => {
+    setRecoveryOption(newOption);
+  };
+
+  const onKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onSendLink();
+    }
+  };
+
+  const onChangeEmailInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setEmailInput({ args: { emailRecovery: { address: value } } });
+  };
+
+  const onChangeSmsInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSmsInput({ args: { smsRecovery: { number: value } } });
+  };
+
+  const isByEmail = (): boolean => {
+    return smsInput.args.smsRecovery.number !== "+502";
+  };
+
+  const isBySms = (): boolean => {
+    return emailInput.args.emailRecovery.address !== "";
+  };
+
+  return (
+    <Box className={classes.homeContainer}>
+      <MobileNavBar />
+      <NavBar />
+      <Container maxWidth="xs">
+        <Box
+          minHeight="100vh"
+          flexDirection="column"
+          display="flex"
+          justifyContent="center"
+        >
+          <ToolbarPadding />
+          <Box mb={4}>
+            <Typography variant="h5" mb={1}>
+              Restablece tu Contraseña
+            </Typography>
+            <Typography>
+              Elige a donde quieres que se envíe el enlace de restablecimiento de
+              contraseña.
+            </Typography>
+          </Box>
+          <Box justifyContent="center">
+            <TabContext value={recoveryOption}>
+              <TabList
+                variant="fullWidth"
+                onChange={handleRecoveryOptionChange}
+                aria-label="Reset Password Methods"
+                centered
+              >
+                <Tab label="Email" value="1" disabled={isByEmail()} />
+                <Tab label="SMS" value="2" disabled={isBySms()} />
+              </TabList>
+              <TabPanel value="1">
+                <Box>
+                  <TextField
+                    fullWidth
+                    label="Email"
+                    variant="outlined"
+                    type="email"
+                    onChange={onChangeEmailInput}
+                    value={emailInput.args.emailRecovery.address}
+                    mb={3}
+                  />
+                  <InputErrorBox error={emailError} />
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    fullWidth
+                    size="large"
+                    onKeyPress={onKeyPress}
+                  >
+                    Enviar
+                  </Button>
+                </Box>
+              </TabPanel>
+              <TabPanel value="2">
+                <Box>
+                  <TextField
+                    fullWidth
+                    label="Número de teléfono"
+                    variant="outlined"
+                    type="tel"
+                    value={smsInput.args.smsRecovery.number}
+                    onChange={onChangeSmsInput}
+                    mb={3}
+                  />
+                  <InputErrorBox error={smsError} />
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    fullWidth
+                    size="large"
+                    onKeyPress={onKeyPress}
+                  >
+                    Enviar
+                  </Button>
+                </Box>
+              </TabPanel>
+            </TabContext>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export const RequestLink = withStyles((theme: Theme) => ({
+  ...styles(theme),
+  homeContainer: {
+    backgroundColor: "white",
+  },
+}))(Component);
