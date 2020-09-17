596a8ae3eaa04cfc0f9ec3d7210ecd821d0c05f4
Ktoxcon <kennethtoxro@gmail.com>
2020-09-16 18:28:50 -0600

    Reset Password screen updated

--- webclient/src/features/accountRecovery/screens/ResetPassword.tsx
+++ webclient/src/features/accountRecovery/screens/ResetPassword.tsx
@@ -1,16 +1,12 @@
 import React from "react";
+import routes from "../../../routes";
 import DependencyContext from "../../../common/contexts/DependencyContext";
 import { styles } from "../../../common/theme";
 import { MobileNavBar, NavBar } from "../components";
+import { MutationResetPasswordArgs } from "@ibexcm/libraries/api";
 import { RouteComponentProps, StaticContext } from "react-router";
-import { Box, Tab, Container, Theme, withStyles, WithStyles } from "@material-ui/core";
-import {
-  Button,
-  InputErrorBox,
-  TextField,
-  ToolbarPadding,
-  Typography,
-} from "../../../common/components";
+import { Box, Container, Theme, withStyles, WithStyles } from "@material-ui/core";
+import { Button, InputErrorBox, TextField, Typography } from "../../../common/components";
 import {
   AccountRecoveryRepositoryInjectionKey,
   ValidationRepositoryInjectionKey,
@@ -25,15 +21,53 @@ const Component: React.FC<Props> = ({ classes, history, location, match, ...prop
     AccountRecoveryRepositoryInjectionKey,
   );
 
-  const [error, setError] = React.useState<Error | null>(null);
+  const [confirmButtonDisabled, setConfirmButtonDisabled] = React.useState<boolean>(true);
+  const [passwordStrengthMessage, setPasswordStrengthMessage] = React.useState<string>("");
+  const [passWordStrengthColor, setPassWordStrengthColor] = React.useState<string>("");
 
-  const onKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
+  const [error, setError] = React.useState<Error | null>(null);
+  const [input, setInput] = React.useState<MutationResetPasswordArgs>({
+    args: { password: "" },
+  });
+  const [confirmPasswordInput, setConfirmPasswordInput] = React.useState<
+    MutationResetPasswordArgs
+  >({
+    args: { password: "" },
+  });
+
+  const onResetPassword = async () => {
+    try {
+    } catch (error) {
+      setError(error);
     }
   };
 
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
+    let { message, color } = ValidationRepository.isPasswordSecure(value);
+
+    setPasswordStrengthMessage(message);
+    setPassWordStrengthColor(color);
+
+    setInput({ args: { password: value } });
+  };
+
+  const onConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    if (value !== input.args.password) {
+      setError(new Error("La contraseñas no coinciden."));
+      setConfirmButtonDisabled(true);
+    } else {
+      setError(null);
+      setConfirmButtonDisabled(false);
+    }
+    setConfirmPasswordInput({ args: { password: value } });
+  };
+
+  const onKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onResetPassword();
+    }
   };
 
   return (
@@ -47,37 +81,50 @@ const Component: React.FC<Props> = ({ classes, history, location, match, ...prop
           display="flex"
           justifyContent="center"
         >
-          <ToolbarPadding />
-          <Box mb={4}>
-            <Typography variant="h5" mb={1}>
-              Restablece tu Contraseña
-            </Typography>
+          <Box flexDirection="column" display="flex" justifyContent="center">
+            <Typography variant="h5">Restablece tu Contraseña</Typography>
             <Typography>Elige una nueva contraseña.</Typography>
           </Box>
-        </Box>
-        <Box mb={4}>
-          <TextField
-            autoFocus
-            fullWidth
-            label="Contraseña"
-            placeholder="Combina varios caracteres"
-            variant="outlined"
-            onKeyPress={onKeyPress}
-            type="password"
-            mb={3}
-          />
-          <TextField
-            fullWidth
-            label="Confirma tu contraseña"
-            variant="outlined"
-            onKeyPress={onKeyPress}
-            type="password"
-            mb={3}
-          />
-          <InputErrorBox error={error} />
-          <Button color="primary" variant="contained" fullWidth size="large">
-            Confirmar
-          </Button>
+          <Box my={2}>
+            <Box>
+              <Typography color={passWordStrengthColor}>
+                {passwordStrengthMessage}
+              </Typography>
+            </Box>
+            <TextField
+              autoFocus
+              fullWidth
+              label="Contraseña nueva"
+              placeholder="Combina varios caracteres"
+              variant="outlined"
+              onKeyPress={onKeyPress}
+              onChange={onInputChange}
+              value={input.args.password}
+              type="password"
+              my={2}
+            />
+            <TextField
+              fullWidth
+              label="Confirma tu contraseña"
+              variant="outlined"
+              onKeyPress={onKeyPress}
+              onChange={onConfirmPasswordChange}
+              value={confirmPasswordInput.args.password}
+              type="password"
+              my={2}
+            />
+            <InputErrorBox error={error} />
+            <Button
+              color="primary"
+              variant="contained"
+              fullWidth
+              size="large"
+              onClick={onResetPassword}
+              disabled={confirmButtonDisabled}
+            >
+              Confirmar
+            </Button>
+          </Box>
         </Box>
       </Container>
     </Box>
