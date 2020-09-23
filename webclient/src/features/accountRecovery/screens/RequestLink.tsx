e538d956dd4e15a8a3c9693be6417e53ed2ac8c6
Ktoxcon <kennethtoxro@gmail.com>
2020-09-23 11:05:32 -0600

    RecoveryOption Enum for tabs implemented

--- webclient/src/features/accountRecovery/screens/RequestLink.tsx
+++ webclient/src/features/accountRecovery/screens/RequestLink.tsx
@@ -14,6 +14,7 @@ import {
   Typography,
 } from "../../../common/components";
 import { QueryRecoverAccountArgs } from "@ibexcm/libraries/api";
+import { RecoveryOption } from "../enum/RecoveryOption";
 
 interface Props extends WithStyles, RouteComponentProps<{}, StaticContext> {}
 
