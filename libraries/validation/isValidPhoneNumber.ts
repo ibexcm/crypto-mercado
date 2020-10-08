568c88b11338b2627fd8b04c741b7b4994447edd
Ktoxcon <kennethtoxro@gmail.com>
2020-10-08 16:24:10 -0600

    Method updated

--- libraries/validation/isValidPhoneNumber.ts
+++ libraries/validation/isValidPhoneNumber.ts
@@ -2,6 +2,14 @@ import { PhoneNumberUtil } from "google-libphonenumber";
 
 export default function isValidPhoneNumber(phoneNumber: string): boolean {
   const Verificator = new PhoneNumberUtil();
+  const phoneNumberCodeGT = "+502";
+
+  if (!Boolean(phoneNumber.length >= 8)) {
+    return false;
+  } else if (Boolean(phoneNumber === phoneNumberCodeGT)) {
+    return false;
+  }
+
   const parsedNumber = Verificator.parse(phoneNumber, "GT");
 
   return Verificator.isValidNumberForRegion(parsedNumber, "GT");
