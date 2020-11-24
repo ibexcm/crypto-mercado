7eb942ae9055f0a2c791819574c050fcc9c2570a
Ktoxcon <kennethtoxro@gmail.com>
2020-11-24 11:55:06 -0600

    Adds BitcoinWebSocketRepository

---
+++ graphqlserver/src/libraries/Crypto/repositories/BitcoinWebSocketRepository.ts
@@ -0,0 +1,44 @@
+import { Observable } from "rxjs";
+import { IMessage } from "stompjs/";
+import { RxStomp } from "@stomp/rx-stomp";
+import { IBitcoinWebSocketRepository } from "../interfaces/IBitcoinWebSocketRepository";
+
+export class BitcoinWebSocketRepository implements IBitcoinWebSocketRepository {
+  private baseUrl = "wss://trade-api.nexus.trade/websocket/v1";
+  private stompProtocolVersion = "1.1";
+
+  StompClient: RxStomp;
+
+  constructor(StompClient: RxStomp) {
+    this.StompClient = StompClient;
+  }
+
+  connect(): void {
+    this.StompClient.activate();
+  }
+
+  async disconnect(): Promise<void> {
+    await this.StompClient.deactivate();
+  }
+
+  getConnectionStatus(): boolean {
+    return this.StompClient.connected();
+  }
+
+  setConnectionConfiguration(authorization?: string): void {
+    this.StompClient.configure({
+      brokerURL: this.baseUrl,
+      connectHeaders: {
+        authorization,
+        "accept-version": this.stompProtocolVersion,
+      },
+      heartbeatIncoming: 10000,
+      heartbeatOutgoing: 10000,
+      reconnectDelay: 5000,
+    });
+  }
+
+  subscribe(path: string, subscriptionHeaders): Observable<IMessage> {
+    return this.StompClient.watch(path, subscriptionHeaders);
+  }
+}
